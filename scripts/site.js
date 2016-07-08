47613a820cab0ae2475fa1f2426cdfa3b896a79f
Cyrus Sarkosh <csarkosh@uw.edu>
2016-07-08 14:06:15 -0700

    Index prepared for templates. Moved index to root

--- scripts/site.js
+++ scripts/site.js
@@ -2,4 +2,15 @@
 
 	var index = angular.module('dm', []);
 
+	index.controller('DisplayController', function() {
+		this.activePage = 1;
+
+		this.setActive = function(number) {
+			this.activePage = number;
+		};
+
+		this.isActive = function(number) {
+			return number === this.activePage;
+		};
+	});
 })();
