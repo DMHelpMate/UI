87e3d4cf0b3bbbbdc5d0f5efd670095b0f4dcbb6
csarkosh <csarkosh@uw.edu>
2016-07-12 00:24:45 -0700

    expanded campaign json example

--- scripts/controllers/BrowseController.js
+++ scripts/controllers/BrowseController.js
@@ -1,7 +1,7 @@
 angular.module('dm')
 .controller('BrowseController', ['$http', '$scope', function($http, $scope) {
-	$scope.hello = 'Hello';
 
+	//Get user's campaigns
 	$http({
 		method: 'GET',
 		url: '../../campaigns/local/campaign1.json'
@@ -11,6 +11,7 @@ angular.module('dm')
 		console.log(err);
 	});
 
+	//Get community's campaigns
 	$http({
 		method: 'GET',
 		url: '../../campaigns/community/campaign2.json'
