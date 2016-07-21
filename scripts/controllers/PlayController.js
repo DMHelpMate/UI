cd5c074ec2bc7134b0fd69862c36a95b3cee4e07
csarkosh <csarkosh@uw.edu>
2016-07-21 11:46:01 -0700

    Enhanced Play UIs interactiveness with campaign json object

--- scripts/controllers/PlayController.js
+++ scripts/controllers/PlayController.js
@@ -1,11 +1,30 @@
-angular.module('dm')
-.controller('PlayController', ['$http', '$scope', 'Campaign', function($http, $scope, Campaign) {
 
-	Campaign.getCamp().then(function(res) {
+angular.module('dm')
+.controller('PlayController', ['$http', '$scope', 'Campaign', function ($http, $scope, Campaign) {
+	
+	//Retrieve campaign object
+	Campaign.getCamp().then( function (res) {
 		$scope.camp = res.data;
-		console.log($scope.camp);
-	}, function(err) {
+	}, function (err) {
 		console.log(err);
 	});
+
+	$scope.activeClass = 'General';
+
+	//Return if param is an object
+	$scope.isObj = function (arr) {
+		return (typeof (arr) === 'object');
+	}
+
+	//Compare the param to active id
+	$scope.isActive = function (id) {
+		console.log(id + " === " + this.activeClass);
+		return (this.activeClass === id);
+	}
+
+	//Set the active var to an id
+	$scope.setActive = function (event) {
+		$scope.activeClass = event.target.id;
+	}
 
 }]);
