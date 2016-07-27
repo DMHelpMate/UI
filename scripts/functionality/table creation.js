281ed32aa62bf954152ed668c0de24dddb99d085
ROBERTPBOYER <robert.pboyer@live.com>
2016-07-27 16:39:26 -0700

    Angular conversion of monster/location

--- scripts/functionality/table creation.js
+++ scripts/functionality/table creation.js
@@ -1,17 +1,20 @@
 
 
 $(document).ready(function () {
+		/*
 		$('#monAdd').click(function () {
 						 var first_row = $('#monRow');
 							 first_row.clone().appendTo('#monTable');
 							 console.log(this);
 		});
+		/*
 		$('#locAdd').click(function () {
 				 var first_row = $('#locRow');
 						first_row.clone().appendTo('#locTable');
 						 console.log(this);
 
  		});
+		*/
 			/* THIS IS FOR WHEN WE ADD UNKNOWN ATTRIBUTES
 			 $('#btnAddCol').click(function () {
 					 $("#blacklistgrid tr").each(function(){
