700f80b13d8563fdde8dc5b40de961ab3dad87d8
Cyrus <csarkosh@uw.edu>
2016-08-14 00:49:40 -0700

    browse now pulls from /campaigns on server

--- scripts/controllers/BrowseController.js
+++ scripts/controllers/BrowseController.js
@@ -21,12 +21,26 @@ angular.module('dm')
 		url: '../../campaigns/community/campaign2.json',
 		headers: {
 			'Cache-Control': 'no-cache',
+			'Content-Type': 'application/json',
 			'Pragma': 'no-cache'
 		}
 	}).then(function(res) {
 		$scope.theirCamps = res.data;
 	}, function(err) {
 		console.log(err);
+	});
+
+	// get all campaigns
+	$http({
+		method: 'GET',
+		url: 'http://api.unicornrampage.com/campaigns',
+		headers: {
+			'Cache-Control': 'no-cache',
+			'Pragma': 'no-cache'
+		}
+	}).then(function(res) {
+		$scope.campaigns = res.data;
+		console.log($scope.campaigns);
 	});
 
 	$scope.getID = function(camp_id , poss) {
