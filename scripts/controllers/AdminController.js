197904b04ace09a83e425a591b136efe98ef18ef
TOWatership <tori.overman@gmail.com>
2016-08-14 20:45:10 -0700

    design changes to admin

--- scripts/controllers/AdminController.js
+++ scripts/controllers/AdminController.js
@@ -2,26 +2,49 @@
 angular.module('dm')
 .controller('AdminController', ['$http', '$scope', function($http, $scope) {
 
-    $scope.onSubmit = function(){
-      console.log("onSubmit start");
-      console.log(this.mon_id_box);
-      console.log(this.enc_id_box);
-      if(this.mon_id_box && this.enc_id_box){
-        $http({
-      		method: 'GET',
-      		url: 'http://api.unicornrampage.com/monsters_encounters?mon_id='+this.mond_id_box+'&enc_id='+this.enc_id_box
+  $scope.onMonSubmit = function(){
+    console.log("onSubmit start");
+    console.log(this.mon_id_box);
+    console.log(this.enc_id_box);
+    if(this.mon_id_box && this.enc_id_box){
+      $http({
+    		method: 'GET',
+    		url: 'http://api.unicornrampage.com/monsters_encounters?mon_id='+this.mond_id_box+'&enc_id='+this.enc_id_box
 
-      	}).then(function success(res) {
-            $scope.m_e_json = angular.toJson(res.data);
-            console.log($scope.m_e_json);
+    	}).then(function success(res) {
+          $scope.m_e_json = angular.toJson(res.data);
+          console.log($scope.m_e_json);
 
-        }, function error(err) {
-            console.log(err);
-        });
-      }
-      else{
-        console.log("onSubmit else");
-      }
-      console.log("onSubmit end");
+      }, function error(err) {
+          console.log(err);
+      });
     }
+    else{
+      console.log("onSubmit else");
+    }
+    console.log("onSubmit end");
+  }
+
+  $scope.onEncSubmit = function(){
+    console.log("onSubmit start");
+    console.log(this.mon_id_box);
+    console.log(this.enc_id_box);
+    if(this.mon_id_box && this.enc_id_box){
+      $http({
+        method: 'GET',
+        url: 'http://api.unicornrampage.com/monsters_encounters?mon_id='+this.mond_id_box+'&enc_id='+this.enc_id_box
+
+      }).then(function success(res) {
+          $scope.m_e_json = angular.toJson(res.data);
+          console.log($scope.m_e_json);
+
+      }, function error(err) {
+          console.log(err);
+      });
+    }
+    else{
+      console.log("onSubmit else");
+    }
+    console.log("onSubmit end");
+  }
 }]);
