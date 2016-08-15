03dc80c294fc07b9205fdc810e419eccb92e08b2
TOWatership <tori.overman@gmail.com>
2016-08-14 20:58:44 -0700

    design changes to admin

--- scripts/controllers/AdminController.js
+++ scripts/controllers/AdminController.js
@@ -2,7 +2,7 @@
 angular.module('dm')
 .controller('AdminController', ['$http', '$scope', function($http, $scope) {
 
-  $scope.onMonSubmit = function(){
+  $scope.onSubmit = function(){
     console.log("onSubmit start");
     console.log(this.mon_id_box);
     console.log(this.enc_id_box);
@@ -12,29 +12,6 @@ angular.module('dm')
     		url: 'http://api.unicornrampage.com/monsters_encounters?mon_id='+this.mond_id_box+'&enc_id='+this.enc_id_box
 
     	}).then(function success(res) {
-          $scope.m_e_json = angular.toJson(res.data);
-          console.log($scope.m_e_json);
-
-      }, function error(err) {
-          console.log(err);
-      });
-    }
-    else{
-      console.log("onSubmit else");
-    }
-    console.log("onSubmit end");
-  }
-
-  $scope.onEncSubmit = function(){
-    console.log("onSubmit start");
-    console.log(this.mon_id_box);
-    console.log(this.enc_id_box);
-    if(this.mon_id_box && this.enc_id_box){
-      $http({
-        method: 'GET',
-        url: 'http://api.unicornrampage.com/monsters_encounters?mon_id='+this.mond_id_box+'&enc_id='+this.enc_id_box
-
-      }).then(function success(res) {
           $scope.m_e_json = angular.toJson(res.data);
           console.log($scope.m_e_json);
 
